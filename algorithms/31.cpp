42e81b88fafd86c87e2889f0ab9854c06a5c352c
DwyaneSuarez <deron120@163.com>
2019-03-21 22:46:08 +0800

    solution of algorithm problem 31

---
+++ algorithms/31.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int len = nums.size();
+        int p = len - 1;
+        while (p > 0) {
+            if (nums[p] <= nums[p - 1])
+                p--;
+            else
+                break;
+        }
+        int tmp = p;
+        if (p > 0) {
+            int t = p - 1;
+            int q = p;
+            while (q < len && nums[q] > nums[t]) q++;
+            swap(nums[t], nums[q - 1]);
+        }
+        int l = tmp, r = len - 1;
+        while (l < r) {
+            swap(nums[l], nums[r]);
+            l++;
+            r--;
+        }
+    }
+};
