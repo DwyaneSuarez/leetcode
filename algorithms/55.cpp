c8489e198139752a6ae198032734c2ed937e4b68
DwyaneSuarez <deron120@163.com>
2019-04-11 06:03:40 +0800

    solution of algorithm problem 55

---
+++ algorithms/55.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int r = 0;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            if (i > r)
+                return false;
+            r = max(r, i + nums[i]);
+        }
+        return r >= n - 1;
+    }
+};
