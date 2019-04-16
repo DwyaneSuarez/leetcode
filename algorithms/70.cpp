40ae38c6db40303f4425e0f99a470963d247c2e9
DwyaneSuarez <deron120@163.com>
2019-04-17 06:31:01 +0800

    solution of algorithm problem 70

---
+++ algorithms/70.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 1)
+            return 1;
+        if (n == 2)
+            return 2;
+        int p = 1;
+        int q = 2;
+        for (int i = 3; i <= n; i++) {
+            int tmp = p + q;
+            p = q;
+            q = tmp;
+        }
+        return q;
+    }
+};
