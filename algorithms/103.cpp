e32e009c1fc33b5d9a4e99f689ef78dad38213fa
DwyaneSuarez <deron120@163.com>
2019-12-18 03:23:47 +0800

    solution of algorithm problem 103

---
+++ algorithms/103.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        queue<pair<TreeNode*, size_t>> que;
+        if (root)
+            que.push({root, 0});
+        while (!que.empty()) {
+            auto head = que.front();
+            que.pop();
+            if (head.second >= ans.size())
+                ans.emplace_back(vector<int>());
+            ans[head.second].emplace_back(head.first->val);
+            if (head.first->left)
+                que.push({head.first->left, head.second + 1});
+            if (head.first->right)
+                que.push({head.first->right, head.second + 1});
+        }
+        for (size_t i = 1; i < ans.size(); i += 2)
+            reverse(ans[i].begin(), ans[i].end());
+        return ans;
+    }
+};
