482fabc661854ff362cd448f86cf4f2880c45516
DwyaneSuarez <deron120@163.com>
2019-12-22 21:58:02 +0800

    solution of algorithm problem 111

---
+++ algorithms/111.cpp
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (!root)
+            return 0;
+        int l = minDepth(root->left);
+        int r = minDepth(root->right);
+        return (l && r) ? 1 + min(l, r) : 1 + l + r;
+    }
+};
