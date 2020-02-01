f4e8f6879270b6f64079a84295539247ccf6e424
DwyaneSuarez <deron120@163.com>
2020-02-01 19:07:42 +0800

    solution of algorithm problem 206

---
+++ algorithms/206.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (!head)
+            return head;
+        ListNode vhead(0);
+        vhead.next = head;
+        ListNode *cur = head;
+        while (cur->next) {
+            ListNode *tmp = cur->next;
+            cur->next = tmp->next;
+            tmp->next = vhead.next;
+            vhead.next = tmp;
+        }
+        return vhead.next;
+    }
+};
