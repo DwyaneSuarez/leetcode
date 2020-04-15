c9a7b246ba9d6686284b58b8b2da262cab8dc53d
DwyaneSuarez <deron120@163.com>
2020-04-15 14:44:24 +0800

    solution of algorithm problem 53

--- algorithms/53.cpp
+++ algorithms/53.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int ans = INT_MIN;
         int n = nums.size();
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            if (sum + nums[i] < 0) {
-                sum = 0;
-                ans = max(ans, nums[i]);
-            } else {
-                sum += nums[i];
-                ans = max(ans, sum);
-            }
+        if (n == 0)
+            return 0;
+        int ans = nums[0];
+        int cur = nums[0];
+        for (int i = 1; i < n; ++i) {
+            if (cur > 0)
+                cur += nums[i];
+            else
+                cur = nums[i];
+            ans = max(ans, cur);
         }
         return ans;
     }
