7eb155e203c19a40bc4ced22fa2db8e47dc29551
menglin.cl <menglin.cl@alibaba-inc.com>
2020-07-15 14:47:57 +0800

    add solution with merge sort

--- offer/51.cpp
+++ offer/51.cpp
@@ -1,3 +1,4 @@
+// BIT
 class Solution {
 public:
     void update(vector<int> &vec, int a, int val) {
@@ -33,5 +34,45 @@ public:
             update(bit, hash[x], 1);
         }
         return ans;
+    }
+};
+
+// MergeSort
+class Solution {
+public:
+    int mergeSort(vector<int> &nums, vector<int> &tmp, int l, int r) {
+        if (l >= r) {
+            return 0;
+        }
+        int mid = (l + r) / 2;
+        int cnt = mergeSort(nums, tmp, l, mid) + mergeSort(nums, tmp, mid + 1, r);
+        int i = l, j = mid + 1, pos = l;
+        while (i <= mid && j <= r) {
+            if (nums[i] <= nums[j]) {
+                cnt += j - (mid + 1);
+                tmp[pos++] = nums[i];
+                ++i;
+            } else {
+                tmp[pos++] = nums[j];
+                ++j;
+            }
+        }
+        while (i <= mid) {
+            cnt += j - (mid + 1);
+            tmp[pos++] = nums[i];
+            ++i;
+        }
+        while (j <= r) {
+            tmp[pos++] = nums[j];
+            ++j;
+        }
+        for (i = l; i <= r; ++i)
+            nums[i] = tmp[i];
+        return cnt;
+    }
+    int reversePairs(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> tmp(n);
+        return mergeSort(nums, tmp, 0, n - 1);
     }
 };
